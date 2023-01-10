81aa9a0240991f05adaa90212dd4f251eea0a21e
unknown <ebussman@gmail.com>
2023-01-09 16:41:54 -0800

    fixed footer to change based on mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,20 @@
 import React from "react";
-import Github from "../assets/images/GitHub_Logo.png"
-import Linkedin from "../assets/images/LI-In-Bug.png"
-
+import Github from "../assets/images/GitHub_Logo.png";
+import Linkedin from "../assets/images/LI-In-Bug.png";
 
 export default function Footer() {
   return (
     <>
-    <div className="footer">
-    <a href ="mailto: ebussman@gmail.com">Email Me!</a>
-      <a href="https://github.com/neft-tk" target="blank">
-        <img src={Github} alt="github" className="icon" />
-      </a>
-      <a href="www.linkedin.com/in/john-erik-buss" target="blank">
-        <img src={Linkedin} alt="linkedin" className="icon" />
-      </a>
+      <div className="footer">
+        <div className="flex flex-row sticky">
+          <a href="mailto: ebussman@gmail.com">Email Me!</a>
+          <a href="https://github.com/neft-tk" target="blank">
+            <img src={Github} alt="github" className="icon" />
+          </a>
+          <a href="www.linkedin.com/in/john-erik-buss" target="blank">
+            <img src={Linkedin} alt="linkedin" className="icon" />
+          </a>
+        </div>
       </div>
     </>
   );
