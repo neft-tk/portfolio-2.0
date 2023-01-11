f011f3650b947ca9a819638551f1076e9ad75b2e
unknown <ebussman@gmail.com>
2023-01-11 14:57:08 -0800

    updating just in case I lose power

--- src/App.js
+++ src/App.js
@@ -1,65 +1,62 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import "./style.css";
 import Nav from "./components/Nav";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import Footer from "./components/Footer";
+import History from "./components/pages/History";
 import Education from "./components/pages/Education";
 import Work from "./components/pages/Work";
 import Contact from "./components/pages/Contact";
-import moment from 'moment'
+import moment from "moment";
 
 function App() {
-  const [dayTime, setDayTime] = useState(moment().format("HH:mm:ss"))
-  const [backgroundClass, setBackgroundClass] = useState("")
+  const [dayTime, setDayTime] = useState(moment().format("HH:mm:ss"));
+  const [backgroundClass, setBackgroundClass] = useState("");
 
   const handleBackgroundChange = (dayTime) => {
-    let dayTimeSplit = dayTime.split(":")
-    
-    let hourDayTime = dayTimeSplit[0]
+    let dayTimeSplit = dayTime.split(":");
 
-    const backgroundDawn = "background-dawn"
-    const backgroundDay = "background-day"
-    const backgroundEvening = "background-evening"
-    const backgroundNight = "background-night"
-    const htmlId = document.getElementById("html")
+    let hourDayTime = dayTimeSplit[0];
 
-    if((hourDayTime >= 6) && (hourDayTime < 9)) {
-      setBackgroundClass(backgroundDawn)
-      htmlId.setAttribute("class", backgroundDawn)
-    } else if((hourDayTime >= 9) && (hourDayTime < 18)) {
-      setBackgroundClass("background-day")
-      htmlId.setAttribute("class", backgroundDay)
-    } else if ((hourDayTime >= 18) && (hourDayTime < 21)) {
-      setBackgroundClass(backgroundEvening)
-      htmlId.setAttribute("class", backgroundEvening)
+    const backgroundDawn = "background-dawn";
+    const backgroundDay = "background-day";
+    const backgroundEvening = "background-evening";
+    const backgroundNight = "background-night";
+    const htmlId = document.getElementById("html");
+
+    if (hourDayTime >= 6 && hourDayTime < 9) {
+      setBackgroundClass(backgroundDawn);
+      htmlId.setAttribute("class", backgroundDawn);
+    } else if (hourDayTime >= 9 && hourDayTime < 18) {
+      setBackgroundClass("background-day");
+      htmlId.setAttribute("class", backgroundDay);
+    } else if (hourDayTime >= 18 && hourDayTime < 21) {
+      setBackgroundClass(backgroundEvening);
+      htmlId.setAttribute("class", backgroundEvening);
     } else {
-      setBackgroundClass(backgroundNight)
-      htmlId.setAttribute("class", backgroundNight)
+      setBackgroundClass(backgroundNight);
+      htmlId.setAttribute("class", backgroundNight);
     }
-
-  }
+  };
 
   useEffect(() => {
-      handleBackgroundChange(dayTime)
+    handleBackgroundChange(dayTime);
   }, []);
 
   return (
     <>
-    <Router>
-      <Nav/>
-      <Routes>
-      <Route path="/" element={<About/>}/>
-      <Route path="/education" element={<Education/>}/>
-      <Route path="/work" element={<Work/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      </Routes>
-    </Router>
-    <Footer/>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/work" element={<Work />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+      <Footer />
     </>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -24,6 +24,14 @@ export default function Nav() {
               </li>
               <li>
                 <a
+                  href="/history"
+                  className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  History
+                </a>
+              </li>
+              <li>
+                <a
                   href="/education"
                   className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
