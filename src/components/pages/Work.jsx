0a1c75313cc174ef28afffb0e47c8a846a1282e4
unknown <ebussman@gmail.com>
2023-01-11 14:31:45 -0800

    updated styling

--- src/components/pages/Work.jsx
+++ src/components/pages/Work.jsx
@@ -20,14 +20,15 @@ export default function Work() {
         <div className="project-card">
           <a href="https://www.pagliacci.com/" target="_blank" rel="noreferrer">
             <h1 className="text-center m-2 text-xl">Pagliacci Pizza</h1>
+            <h2 className="underline">Delivery Driver Manager</h2>
             <ul>
-              <li className="text-sm">
+              <li>
                 Managed workers’ schedules, training, and performance reviews
               </li>
-              <li className="text-sm">
+              <li>
                 Contributed to long-term performance growth plans company wide
               </li>
-              <li className="text-sm">
+              <li>
                 One of the fastest promoted workers recorded, multiple
                 performance based record holder
               </li>
@@ -37,27 +38,32 @@ export default function Work() {
         <div className="project-card">
         <a href="https://www.brplusa.com/" target="_blank" rel="noreferrer">
           <h1 className="text-center m-2 text-xl">BR+A Consulting Engineers</h1>
+          <h2 className="underline">Construction Administrator</h2>
           <ul>
-            <li className="text-sm">Worked for architects, engineers, and various clients for processing paperwork</li>
-            <li className="text-sm">Edited outgoing emails, reports, and presentations from the company to clients</li>
+            <li>Worked for architects, engineers, and various clients for processing paperwork</li>
+            <li>Edited outgoing emails, reports, and presentations from the company to clients</li>
           </ul>
         </a>
         </div>
         <div className="project-card">
           <a href="https://atc.fas.harvard.edu/" target="_blank" rel="noreferrer">
           <h1 className="text-center m-2 text-xl">Harvard Assistive Technology Center</h1>
+          <h2 className="underline">Product Editor</h2>
           <ul>
-            <li className="text-sm">Scanned textbooks into audio files for students with learning difficulties</li>
-            <li className="text-sm">Worked independently as well as managed long-term projects</li>
+            <li>Scanned textbooks into audio files for clients with learning difficulties</li>
+            <li>Worked independently as well as managed long-term projects</li>
+            <li>Multiple semesters worth of time, was asked to work longer</li>
           </ul>
           </a>
         </div>
         <div className="project-card">
           <a href="https://dignitas.gg/" target="_blank" rel="noreferrer">
             <h1 className="text-center m-2 text-xl">Dignitas</h1>
+            <h2 className="underline">Esports Freelance Writer</h2>
             <ul>
-              <li className="text-sm">Worked on articles of my choosing as well as paid content</li>
-              <li className="text-sm">Conduct interviews with esports professionals</li>
+              <li>Worked on articles of my choosing as well as paid content</li>
+              <li>Conducted interviews with esports professionals</li>
+              <li>Self-managed my own appropriate deadlines</li>
             </ul>
           </a>
         </div>
