0e6551a56e165a08be40c4c9e31123d7cf86d782
unknown <ebussman@gmail.com>
2023-01-12 13:25:08 -0800

    added start of commit history page but ran out of API calls

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,12 +7,13 @@ export default function Project({ projectName, repoLink, deployedLink, projectDe
       <a
         href={deployedLink}
         target="_blank"
+        rel="noreferrer"
         className="project-card"
       >
         <h5 class="mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {projectName}
         </h5>
-        <a href={repoLink} target="_blank">
+        <a href={repoLink} target="_blank" rel="noreferrer">
           <span className="border-2 border-gray-500 p-1 rounded ">GitHub Repo</span>
         </a>
         <p className="my-4 font-normal text-gray-700 dark:text-gray-400">
--- src/components/pages/History.jsx
+++ src/components/pages/History.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useEffect } from "react";
+
 
 export default function History() {
-    return (
-      <div>History</div>
-    )
+
+  function getApi() {
+    // replace `octocat` with anyone else's GitHub username
+    var requestUrl = 'https://api.github.com/users/neft-tk/repos';
+  
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data)
+        let repoData = data.map(repo => 
+        //   {
+        //   fetch(`https://api.github.com/users/neft-tk/${repo.name}/commits/`)
+        //   .then(function (response) {
+        //     return response.json();
+        //   })
+        //   .then(function(commitData) {
+        //     console.log(commitData)
+        //   })
+        // }
+        console.log(repo.name)
+        )
+        console.log(repoData)
+      });
+  }
+
+  useEffect(() => {
+    getApi()
+  },[]);
+
+  return (
+    <>
+      <div>
+        <h1>History</h1>
+      </div>
+    </>
+  );
 }
