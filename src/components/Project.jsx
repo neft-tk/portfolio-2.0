d6f478eb8b051697934aa09e30784c289752f8e8
Erik <ebussman@gmail.com>
2024-04-04 19:55:58 -0700

    updated color styling for project cards

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -10,13 +10,13 @@ export default function Project({ projectName, repoLink, deployedLink, projectDe
         rel="noreferrer"
         className="project-card"
       >
-        <h5 class="mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        <h5 className="mb-4 text-2xl font-bold tracking-tight">
           {projectName}
         </h5>
         <a href={repoLink} target="_blank" rel="noreferrer">
           <span className="border-2 border-gray-500 p-1 rounded ">GitHub Repo</span>
         </a>
-        <p className="my-4 font-normal text-gray-700 dark:text-gray-400">
+        <p className="my-4 font-normal text-gray-700">
             {projectDescription}
         </p>
       </a>
